b7041e134a1b75c697e7ee9a81ea328fa56a3e70
Frosina Remenska <frosina.remenska@netcetera.com>
2019-03-16 17:33:50 +0100

    setup skeleton

---
+++ beer-with-us-react-app/src/components/beer/BeerDetail.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './BeerDetail.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="BeerDetail">
+                <header className="BeerDetail-hgeader">
+                    <p>
+                    Beer Detail Page
+                    </p>
+                </header>
+            </div>
+    );
+    }
+}
+
+export default BeerDetails;
---
+++ beer-with-us-react-app/src/components/login/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="Login">
+            <p>
+                Login Page
+            </p>
+        </div>
+    );
+    }
+}
+
+export default Login;
